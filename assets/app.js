9d6fe54bbc97228ff444f53c8f1336b14dc2527c
Yuliia Savdeieva <yuliia.savdeieva@Yuliias-MacBook-Pro.local>
2020-04-15 15:19:41 +0300

    rendered current weather information and changed some ui

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,26 @@
 const get_loc = new getLocation;
 const api = new API;
+const ui = new UI;
 
 
 get_loc.getCurrentLocation((lat, lon) => {
     api.getPostion(lat, lon)
-        .then(result => console.log(result));
+        .then(result => {
+            ui.createMainInfo({
+                city: result.name,
+                country: result.sys.country,
+                temp: result.main.temp,
+                weather: result.weather[0].description,
+                icon: result.weather[0].icon,
+            });
+            ui.createAddInfo({
+                humidity: result.main.humidity,
+                feels: result.main.feels_like,
+                wind_deg: result.wind.deg,
+                wind_speed: result.wind.speed
+            });
+            //console.log(result);
+            
+        })
+        .catch(err => err);
 })
---
+++ assets/ui.js
@@ -0,0 +1,40 @@
+class UI{
+    constructor(){
+        this.divCard = document.querySelector('.card-body');
+    }
+
+    createMainInfo(data){
+        console.log(data);
+        this.divCard.innerHTML =  `
+            <div class="container container-card">
+                <h1 class="card-title">${data.city}, ${data.country}</h1>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4 class="card-subtitle text-muted">${data.weather}</h4>
+                        <h4 class="card-subtitle mt-3">${data.temp}</h4>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="card-img" src="https://openweathermap.org/img/wn/${data.icon}@2x.png">
+                    </div>
+                </div>
+
+                <ul class="list-group"></ul>
+            </div>
+            
+
+        `;
+        
+    }
+
+    createAddInfo(data){
+        console.log(data);
+        const ul = document.querySelector('.list-group');
+        ul.innerHTML = `
+            <li class="">Fells Like: ${data.feels}</li>
+            <li class="">Humidity: ${data.humidity}</li>
+            <li class="">Wind from the ${data.wind_deg} at ${data.wind_speed}</li>
+        `
+        
+    }
+}
+
