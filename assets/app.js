ad2f57ca5d8f93ca2fa69fc8cb2787b1a3a42bac
Yuliia Savdeieva <yuliia.savdeieva@Yuliias-MacBook-Pro.local>
2020-04-15 15:42:06 +0300

    ui was changed, preloader eas added

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,43 @@ const get_loc = new getLocation;
 const api = new API;
 const ui = new UI;
 
+// Converting degrees into wind name
+const degToCard = (deg) => {
+    if (deg > 11.25 && deg < 33.75) {
+        return "N/NE";
+    } else if (deg > 33.75 && deg <= 56.25) {
+        return "E/NE";
+    } else if (deg > 56.25 && deg <= 78.75) {
+        return "E";
+    } else if (deg > 78.75 && deg <= 101.25) {
+        return "E/SE";
+    } else if (deg > 101.25 && deg <= 123.75) {
+        return "E/SE";
+    } else if (deg > 123.75 && deg <= 146.25) {
+        return "SE";
+    } else if (deg > 146.25 && deg <= 168.75) {
+        return "S/SE";
+    } else if (deg > 168.75 && deg <= 191.25) {
+        return "S";
+    } else if (deg > 191.25 && deg <= 213.75) {
+        return "S/SW";
+    } else if (deg > 213.75 && deg <= 236.25) {
+        return "SW";
+    } else if (deg > 236.25 && deg <= 258.75) {
+        return "W/SW";
+    } else if (deg > 258.75 && deg <= 281.25) {
+        return "W";
+    } else if (deg > 281.25 && deg <= 303.75) {
+        return "W/NW";
+    } else if (deg > 303.75 && deg <= 326.25) {
+        return "NW";
+    } else if (deg > 326.25 && deg <= 348.75) {
+        return "N/NW";
+    } else {
+        return "N";
+    }
+}
+
 
 get_loc.getCurrentLocation((lat, lon) => {
     api.getPostion(lat, lon)
@@ -9,18 +46,17 @@ get_loc.getCurrentLocation((lat, lon) => {
             ui.createMainInfo({
                 city: result.name,
                 country: result.sys.country,
-                temp: result.main.temp,
+                temp: (result.main.temp - 273).toFixed(),
                 weather: result.weather[0].description,
                 icon: result.weather[0].icon,
             });
             ui.createAddInfo({
                 humidity: result.main.humidity,
-                feels: result.main.feels_like,
-                wind_deg: result.wind.deg,
+                feels: (result.main.feels_like - 273).toFixed(),
+                wind_deg: degToCard(result.wind.deg),
                 wind_speed: result.wind.speed
             });
-            //console.log(result);
-            
+
         })
         .catch(err => err);
 })
--- assets/ui.js
+++ assets/ui.js
@@ -11,14 +11,18 @@ class UI{
                 <div class="row">
                     <div class="col-md-6">
                         <h4 class="card-subtitle text-muted">${data.weather}</h4>
-                        <h4 class="card-subtitle mt-3">${data.temp}</h4>
+                        <h4 class="card-subtitle mt-3">${data.temp}&deg;C</h4>
                     </div>
                     <div class="col-md-6">
                         <img class="card-img" src="https://openweathermap.org/img/wn/${data.icon}@2x.png">
                     </div>
                 </div>
 
-                <ul class="list-group"></ul>
+                <ul class="list-group mb-5 text-white"></ul>
+
+                <button type="button" class="btn btn-outline-primary">Change Location</button>
+
+
             </div>
             
 
@@ -30,9 +34,9 @@ class UI{
         console.log(data);
         const ul = document.querySelector('.list-group');
         ul.innerHTML = `
-            <li class="">Fells Like: ${data.feels}</li>
-            <li class="">Humidity: ${data.humidity}</li>
-            <li class="">Wind from the ${data.wind_deg} at ${data.wind_speed}</li>
+            <li class="">Fells Like: ${data.feels}&deg;C</li>
+            <li class="">Humidity: ${data.humidity}%</li>
+            <li class="">Wind from the ${data.wind_deg} at ${data.wind_speed} MPH</li>
         `
         
     }
